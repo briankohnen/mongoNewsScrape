ad367835dba1c5b664437ce7e2613f2c6981fc06
BrianKohnen <briankohnen@yahoo.com>
2019-10-18 16:07:45 -0500

    bk--added article schema. added logic onto /scrap in apiRoutes so that duplicate articles are not saved

---
+++ models/Article.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema({
+    image: {
+        type: String,
+        required: false
+    },
+    link: {
+        type: String,
+        require: true
+    },
+    headline: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    comment: {
+        type: Schema.Types.ObjectId,
+        ref: "Comment"
+    }
+});
+
+const Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,22 +8,34 @@ module.exports = function(app) {
         axios.get('https://chicago.suntimes.com/entertainment-and-culture').then(function(response) {
             let $ = cheerio.load(response.data);
 
-            let results = [];
             $('div.c-compact-river__entry').each(function(i, element) {
+                let articleResult = {};
+
                 let image = $(element).find('noscript').text().split(' ').pop().slice(5, -2);
                 let headline = $(element).find('h2.c-entry-box--compact__title').find('a').text().trim();
                 let link = $(element).find('h2.c-entry-box--compact__title').find('a').attr('href').trim();
                 let summary = $(element).find('p.p-dek').text().trim();
                 let category = $(element).find('ul').find('span').text().trim();
-                results.push({
-                    image: image,
-                    headline: headline,
-                    link: link,
-                    summary: summary,
-                    category: category
+
+                articleResult.image = image;
+                articleResult.link = link;
+                articleResult.headline = headline;
+                articleResult.summary = summary;
+                articleResult.category = category;
+
+                db.Article.findOne({headline: headline}).then(function(checkIfDup) {
+                    if (checkIfDup) {
+                        console.log('duplicate article');
+                        return;
+                    } else {
+                        db.Article.create(articleResult).then(function(newArticle) {
+                            console.log(newArticle);
+                        }).catch(function(err) {
+                            console.log(err);
+                        });
+                    }
                 });
             });
-            console.log(results);
         });
     });
 };
